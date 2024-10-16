854caeda93a267366bbd9c05e053acd8e87d8843
iiTzX1sRol <vovavovakak@gmail.com>
2024-10-16 17:46:09 +0300

    web6 js fix

--- web6/js/script.js
+++ web6/js/script.js
@@ -25,14 +25,14 @@ const products = {
         "price": 499,
         "subproducts": [],
         "subproperties": [
-			{
-                "name": "Керамо-гранитная",
-                "multiplier": 1.25
+            {
+                "multiplier": 1.25,
+                "name": "Керамо-гранитная"
             },
-			{
-				"name": "Мозаичная",
-				"multiplier": 3.75
-			}
+            {
+                "multiplier": 3.75,
+                "name": "Мозаичная"
+            }
         ]
     },
     "p4": {
@@ -49,13 +49,12 @@ const products = {
         ],
         "subproperties": [
             {
-                "name": "С системой быстрой вентиляции воздуха",
-                "multiplier": 1.45
+                "multiplier": 1.45,
+                "name": "С системой быстрой вентиляции воздуха"
             }
         ]
     }
-}
-
+};
 function loadSubproductsOptions(subproducts, subproductsSelectEl) {
     if (subproducts.length > 0) {
         subproductsSelectEl.removeAttribute("disabled");
@@ -63,7 +62,6 @@ function loadSubproductsOptions(subproducts, subproductsSelectEl) {
     else {
         subproductsSelectEl.setAttribute("disabled", "");
     }
-    // Добавление сортов
     subproducts.forEach(subproduct => {
         let option = document.createElement("option");
         let name = document.createTextNode(subproduct["name"]);
@@ -72,42 +70,28 @@ function loadSubproductsOptions(subproducts, subproductsSelectEl) {
         subproductsSelectEl.appendChild(option);
     });
 }
-
 function changeProductType() {
     let subproductsEl = document.getElementById("subproducts");
     let typeSelectEls = document.getElementsByName("product-type");
     let selectedType;
-
-    // Ищем выбранный продукт
     typeSelectEls.forEach(typeSelectEl => {
         if (typeSelectEl.checked === true) {
             selectedType = typeSelectEl.value;
         }
     });
     let product = products[selectedType];
-
     let subproductsSelectEl = document.getElementById("subproducts-select");
-    let subproductsCheckboxes = document.getElementById("subproduct-properties");
-    
+    let subproductsCheckboxes =
+        document.getElementById("subproduct-properties");
     subproductsSelectEl.innerHTML = " ";
-
-    // Создание стандартного выбора в селекте
     let option = document.createElement("option");
     let name = document.createTextNode("Выбрать");
     option.setAttribute("value", "");
     option.appendChild(name);
     subproductsSelectEl.appendChild(option);
-
-    // Добавление сортов в селект
     let subproducts = product["subproducts"];
     loadSubproductsOptions(subproducts, subproductsSelectEl);
-    
-
-    // Добавление доп услуг
     let subproductProperties = product["subproperties"];
-
-    
-    //^^
     subproductsCheckboxes.innerHTML = " ";
     if (Object.keys(subproductProperties).length > 0) {
         let index = 0;
@@ -120,72 +104,51 @@ function changeProductType() {
             cbInputEl.id = "product-property-" + index;
             cbInputEl.value = sProperty.multiplier;
             checkboxDiv.appendChild(cbInputEl);
-
             let cbLabel = document.createElement("label");
             cbLabel.setAttribute("for", "product-property-" + index);
             cbLabel.id = "product-property-label-" + index;
             cbLabel.innerText = sProperty.name;
             checkboxDiv.appendChild(cbLabel);
-
             subproductsCheckboxes.appendChild(checkboxDiv);
             index++;
         });
     }
-    //^^
-    
 }
-
 function calculate() {
     let quantityEl = document.getElementById("quantity-input");
     let typeSelectEls = document.getElementsByName("product-type");
-
-    // Ищем выбранный тип
     let selectedType;
     typeSelectEls.forEach(typeSelectEl => {
         if (typeSelectEl.checked === true) {
             selectedType = typeSelectEl.value;
         }
     });
-
-    // Проверка, выбран ли товар
     if (!selectedType) {
         alert("Пожалуйста, выберите товар из списка.");
-        return; // Выход из функции, если товар не выбран 
-		}
-
+        return;
+    }
     let product = products[selectedType];
-    let subproductTypeEl = document.getElementById("subproducts-select");
-
-    // Проверка наличия сабпродуктов 
-	if (subproductTypeEl && product.subproducts && product.subproducts.length > 0) {
-        // Если сабпродукты есть, проверяем, выбран ли сабпродукт
+    let subproductTypeEl =
+        document.getElementById("subproducts-select");
+    if (subproductTypeEl &&
+        product.subproducts &&
+        product.subproducts.length > 0) {
         if (subproductTypeEl.value === "") {
             alert("Пожалуйста, выберите уточнение из списка.");
-            return; // Выход из функции, если сабпродукт не выбран
+            return;
         }
     }
-
-    // Получаем количество 
-	let quantity = parseInt(quantityEl.value);
-
-    // Проверка введенного количества 
-	if (isNaN(quantity) || quantity <= 0) {
+    let quantity = parseInt(quantityEl.value);
+    if (isNaN(quantity) || quantity <= 0) {
         alert("Пожалуйста, введите корректное количество.");
-        return; // Выход из функции, если количество некорректно
+        return;
     }
-
-    // Получаем цену товара
     let productPrice = product.price;
-
-    // Если сабпродукт выбран, используем его цену 
-	if (subproductTypeEl && subproductTypeEl.value) {
+    if (subproductTypeEl && subproductTypeEl.value) {
         productPrice = subproductTypeEl.value;
     }
-
     let result = quantity * productPrice;
-
-    // Проверка на дополнительные свойства 
-	let subproductProperties = product.subproperties;
+    let subproductProperties = product.subproperties;
     if (Object.keys(subproductProperties).length > 0) {
         let index = 0;
         subproductProperties.forEach(sProperty => {
@@ -197,36 +160,27 @@ function calculate() {
             index++;
         });
     }
-
-    // Вывод результата 
-	let resultEl = document.getElementById("result");
+    let resultEl = document.getElementById("result");
     resultEl.innerHTML = result.toFixed(0); // Без дробной части выводим
 }
-
 document.addEventListener("DOMContentLoaded", (event) => {
-    // Добавление типов продуктов
     let productsEl = document.getElementById("calc-radio-group");
     for (let i = 1; i <= Object.keys(products).length; i++) {
         let productDivEl = document.createElement("div");
-
         let productRadioEl = document.createElement("input");
         productRadioEl.setAttribute("type", "radio");
         productRadioEl.setAttribute("id", `radio-${i}`);
         productRadioEl.setAttribute("value", `p${i}`);
         productRadioEl.setAttribute("name", "product-type");
         productDivEl.appendChild(productRadioEl);
-
         let productRadioLabelEl = document.createElement("label");
         productRadioLabelEl.setAttribute("for", `radio-${i}`);
         productRadioLabelEl.innerText = products[`p${i}`]["name"];
         productDivEl.appendChild(productRadioLabelEl);
-
         productsEl.appendChild(productDivEl);
     }
-
     let selectEl = document.getElementById("calc-radio-group");
     selectEl.addEventListener("change", changeProductType);
-
     let buttonEl = document.getElementById("calc-button");
     buttonEl.addEventListener("click", calculate);
 });
